0b9f0af854c7242fc7ddcaa20685c6d5c1fe9a2b
lucasguo086 <lucas.guo@autuni.ac.nz>
2023-05-16 15:03:11 +1200

    beijing_taxi

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 /home/lucasguo/miniconda3/envs/Bigscity/bin/python \
-run_model.py --task traj_loc_pred --model DeepMove --dataset foursquare_nyc
+run_model.py --task traj_loc_pred --model DeepMove --dataset beijing_taxi
